3661d35c32eab820876bced85bcac8cbb58e0685
rzatalibov555 <sethubpro@gmail.com>
2023-10-14 19:24:28 +0400

    bugun js ders 1 idi

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+// console.log("MEN BURDAYAM ABI!")
+
+// document.write("Salam Abi necesen?")
+
+// window.alert("Programming")
+
+// document.getElementById("demo").innerHTML = "Welcome"
+
+// prompt("IStifadeci adini daxi edin:")
+
+// confirm("DERS oyrenmek isteyirsenmi?")
+
+// window.print()
+
+// let a = confirm("DERS oyrenmek isteyirsenmi?")
+
+// if(a){
+//     window.location.href = "https://www.w3schools.com/js/js_output.asp";
+// }else{
+//     window.location.href = "https://www.youtube.com/watch?v=38akWd3JXkM&ab_channel=FaridAgha-Topic";
+// }
+
